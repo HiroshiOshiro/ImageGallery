327b83db1b00a644b4c661db85dab5ac4433801b
hiroshi oshiro <ooshiro010@gmail.com>
2019-06-30 06:55:41 +0900

    add saving image function

--- ImageGalleryWithFlicker/ImageGalleryWithFlicker/Controller/PhotoDetailViewController.swift
+++ ImageGalleryWithFlicker/ImageGalleryWithFlicker/Controller/PhotoDetailViewController.swift
@@ -48,4 +48,47 @@ class PhotoDetailViewController: UIViewController {
         descriptionWebView.loadHTMLString(photo?.description ?? "", baseURL: nil)
         descriptionBaseView.addSubview(descriptionWebView)
     }
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+        let alert = UIAlertController(title: "", message: "Do you want to save this image?", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: {(action) in
+            self.saveImage()
+        })
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil )
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func saveImage() {
+        if let image : UIImage = imageView.image {
+            // saving image
+            UIImageWriteToSavedPhotosAlbum(image,
+                                           self,
+                                           #selector(self.didFinishSavingImage(_:didFinishSavingWithError:contextInfo:)),
+                                           nil)
+        }
+    }
+    
+    @objc func didFinishSavingImage(_ image: UIImage, didFinishSavingWithError error: NSError!, contextInfo: UnsafeMutableRawPointer) {
+        // show message depend on result
+        var message = ""
+        if error != nil {
+            message = "Faild to save. Please chack permisssion and strage capacity."
+        } else {
+            message = "Saved successfully!"
+        }
+        
+        let alertController = UIAlertController(title: "", message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func getDocumentsURL() -> NSURL {
+        let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0] as NSURL
+        return documentsURL
+    }
 }
